18383c70a4973088813b0579c3da9bbe2043a2bd
Leonardo Alaff <leonardoalaff@gmail.com>
2024-01-02 19:55:19 -0300

    1.1.1

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let iconeMenu = document.querySelector('.icone-menu')
-let ul = document.querySelector('ul')
+let ul = document.querySelector('.ul')
+let nav = document.querySelector('.nav')
 
 iconeMenu.addEventListener('click', () => {
     iconeMenu.classList.toggle('active')
     ul.classList.toggle('active')
+})
+
+window.addEventListener('scroll', () => {
+    nav.classList.toggle('active', scrollY > 100)
 })
