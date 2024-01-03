e5c781e62318610c6f745c396a9bc9bd1a072901
Leonardo Alaff <leonardoalaff@gmail.com>
2024-01-03 01:27:13 -0300

    1.1.2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let iconeMenu = document.querySelector('.icone-menu')
 let ul = document.querySelector('.ul')
 let nav = document.querySelector('.nav')
+let horario = document.querySelector('.horario')
+
+let h2Horario = document.querySelector('.h2-horario')
+let h2Manicure = document.querySelector('.h2-manicure')
+let h2Pedicure = document.querySelector('.h2-pedicure')
+
+let pHorario = document.querySelector('.p-horario')
+let p2Horario = document.querySelector('.p2-horario')
+let p2Manicure = document.querySelector('.p2-manicure')
+let p2Pedicure = document.querySelector('.p2-pedicure')
+
+let ih1 = document.querySelector('.ih1')
+let ih2 = document.querySelector('.ih2')
+
 
 iconeMenu.addEventListener('click', () => {
     iconeMenu.classList.toggle('active')
@@ -9,4 +23,51 @@ iconeMenu.addEventListener('click', () => {
 
 window.addEventListener('scroll', () => {
     nav.classList.toggle('active', scrollY > 100)
+})
+
+ih1.addEventListener('mouseenter', () => {
+    h2Horario.classList.toggle('manicure')
+    pHorario.classList.toggle('manicure')
+    p2Horario.classList.toggle('manicure')
+    horario.classList.toggle('manicure')
+
+    ih1.classList.toggle('active')
+    h2Manicure.classList.toggle('active')
+    p2Manicure.classList.toggle('active')
+})
+ih1.addEventListener('mouseout', () => {
+    h2Horario.classList.toggle('manicure')
+    pHorario.classList.toggle('manicure')
+    p2Horario.classList.toggle('manicure')
+    horario.classList.toggle('manicure')
+
+    ih1.classList.toggle('active')
+    h2Manicure.classList.toggle('active')
+    p2Manicure.classList.toggle('active')
+})
+
+
+ih2.addEventListener('mouseenter', () => {
+
+    h2Horario.classList.toggle('pedicure')
+    pHorario.classList.toggle('pedicure')
+    p2Horario.classList.toggle('pedicure')
+    horario.classList.toggle('pedicure')
+
+    ih2.classList.toggle('active')
+    h2Pedicure.classList.toggle('active')
+    p2Pedicure.classList.toggle('active')
+    
+})
+ih2.addEventListener('mouseout', () => {
+
+    h2Horario.classList.toggle('pedicure')
+    pHorario.classList.toggle('pedicure')
+    p2Horario.classList.toggle('pedicure')
+    horario.classList.toggle('pedicure')
+
+    ih2.classList.toggle('active')
+    h2Pedicure.classList.toggle('active')
+    p2Pedicure.classList.toggle('active')
+    
 })
