f6ad8985d28bbb8cdb97441dd7e07aac032807c2
Leonardo Alaff <leonardoalaff@gmail.com>
2024-01-06 15:35:29 -0300

    1.7

--- script.js
+++ script.js
@@ -4,24 +4,12 @@ let nav = document.querySelector('.nav')
 let horario = document.querySelector('.horario')
 
 let h2Horario = document.querySelector('.h2-horario')
-let h2Manicure = document.querySelector('.h2-manicure')
-let h2Pedicure = document.querySelector('.h2-pedicure')
 
 let pHorario = document.querySelector('.p-horario')
 let p2Horario = document.querySelector('.p2-horario')
-let p2Manicure = document.querySelector('.p2-manicure')
-let p2Pedicure = document.querySelector('.p2-pedicure')
-
-let ih1 = document.querySelector('.ih1')
-let ih2 = document.querySelector('.ih2')
 
 let iconeWPP = document.querySelector('.icone-wpp')
 let sessao2 = document.querySelector('#sessao2')
-let sessao3 = document.querySelector('#sessao3')
-let sessao4 = document.querySelector('#sessao4')
-let sessao5 = document.querySelector('#sessao5')
-let sessao6 = document.querySelector('#sessao6')
-let sessao7 = document.querySelector('#sessao7')
 let aberto = document.querySelector('.aberto')
 let fechado = document.querySelector('.fechado')
 let seta = document.querySelector('.seta')
@@ -41,20 +29,6 @@ window.addEventListener('scroll', () => {
     nav.classList.toggle('verduras', scrollY > 449) 
     sessao2.classList.toggle('active', scrollY > 450) 
 
-    nav.classList.toggle('legumes', scrollY > 1199)
-    sessao3.classList.toggle('active', scrollY > 1200)
-
-    nav.classList.toggle('frutas', scrollY > 1949)
-    sessao4.classList.toggle('active', scrollY > 1950)
-
-    nav.classList.toggle('bebidas', scrollY > 2699)
-    sessao5.classList.toggle('active', scrollY > 2700)
-
-    nav.classList.toggle('sorvetes', scrollY > 3449)
-    sessao6.classList.toggle('active', scrollY > 3450)
-
-    nav.classList.toggle('acai', scrollY > 4249)
-    sessao7.classList.toggle('active', scrollY > 4250)
     seta.classList.toggle('active', scrollY > 450)
 })
 
@@ -63,18 +37,15 @@ window.addEventListener('load', () => {
     const diaSemana = agora.getDay();
     const hora = agora.getHours();
 
+    setTimeout(() => {
+        anuncio.classList.toggle('active')
+    }, 3000); // 1000 milissegundos = 1 segundo
+
     if (diaSemana >= 1 && diaSemana <= 5 && hora >= 8 && hora < 18) {
         return aberto.classList.toggle('active');
     } else {
         return fechado.classList.toggle('active');
     }
-})
-
-
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        anuncio.classList.toggle('active')
-    }, 3000); // 1000 milissegundos = 1 segundo
 })
 
 
