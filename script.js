c0b7a39ac1c05c094e2d614c6bb5b46e205a4a4e
Leonardo Alaff <leonardoalaff@gmail.com>
2024-03-08 15:52:40 -0300

    3.8.4

--- script.js
+++ script.js
@@ -111,3 +111,29 @@ function updateClock() {
   
   // Inicializa o relógio ao carregar a página
   updateClock();
+
+
+  /**** SLIDE ****/
+  let slideIndex = 0;
+  showSlide(slideIndex);
+  
+  function moveSlide(n) {
+      showSlide(slideIndex += n);
+  }
+  
+  function showSlide(n) {
+      const slides = document.getElementsByClassName("slide");
+      
+      if (n >= slides.length) {
+          slideIndex = 0;
+      }
+      if (n < 0) {
+          slideIndex = slides.length - 1;
+      }
+      
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      
+      slides[slideIndex].style.display = "block";  
+  }
